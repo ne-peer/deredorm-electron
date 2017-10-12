652b88b9c9e5eabe40ad25563fb93428489633d8
ne-peer <mileage0110@gmail.com>
2017-10-12 11:50:12 +0900

    [add] startup page

---
+++ index.js
@@ -0,0 +1,31 @@
+// ./main.js
+const {app, BrowserWindow} = require('electron')
+
+let win = null;
+
+app.on('ready', function () {
+
+  // Initialize the window to our specified dimensions
+  win = new BrowserWindow({width: 1000, height: 600});
+
+  // Specify entry point
+  win.loadURL('http://deredorm.ne-peer.com');
+
+  // Remove window once app is closed
+  win.on('closed', function () {
+    win = null;
+  });
+
+});
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
